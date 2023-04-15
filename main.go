3b62c3924418172d33443bce34e277db1184f746
kurehajime <xiidec@gmail.com>
2023-04-15 20:05:00 +0900

    fix io/ioutil

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -42,7 +42,7 @@ func handle(w http.ResponseWriter, r *http.Request) {
 
 	defer resp.Body.Close()
 	// レスポンスボディを読み取る
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
